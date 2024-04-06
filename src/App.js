16bd94eab5554eec788fbaf5888cc331f4b15c6c
drisyx <drisyashenoyd@gmail.com>
2024-04-06 17:52:11 +0530

    View  Recipe Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
 import SearchRecipe from './components/SearchRecipe';
+import ViewRecipe from './components/ViewRecipe';
 
 function App() {
   return (
     <div>
     <AddRecipe/>
     <SearchRecipe/>
+    <ViewRecipe/>
     </div>
   );
 }
---
+++ src/components/ViewRecipe.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const ViewRecipe = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card" >
+                                    <img src="https://www.modernhoney.com/wp-content/uploads/2018/10/Soft-Chewy-Peanut-Butter-Cookies-4-cr.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">PEANUT BUTTER COOKIES</h5>
+                                        <p class="card-text">Whip up these scrumptious 3-Ingredient Peanut Butter Cookies in a snap! With just peanut butter, sugar, and an egg, you'll have a delightful treat that's perfect for satisfying your sweet tooth.</p>
+                                        <a href="#" class="btn btn-primary">view</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card" >
+                                    <img src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/106358.jpg?resize=600:*&output-format=auto&output-quality=auto" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">CRÈME BRÛLÉE</h5>
+                                        <p class="card-text">Try this classic dessert that is easy to make and requires only a few ingredients. Rich and creamy custard is topped with a crunchy layer of caramelized sugar, making it the perfect indulgent treat.</p>
+                                        <a href="#" class="btn btn-primary">view</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+
+                                <div class="card" >
+                                    <img src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/5d665ff40cff48a9a61006e28546d56d/dessertsfinalFB.jpg?resize=600:*&output-format=auto&output-quality=auto" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">COOKIES AND CREAM MOUSSE</h5>
+                                        <p class="card-text">Whip up this dreamy Cookies and Cream Mousse for a delightful dessert experience! With a heavenly blend of crushed Oreos and creamy mousse, your taste buds will be dancing with joy.</p>
+                                        <a href="#" class="btn btn-primary">view</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div class="card" >
+                                    <img src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/059f469b91e144828596039bfd763ced/BFV12887_4Easy3-IngredientSideDishes-FB1080SQ.jpg?resize=600:*&output-format=auto&output-quality=auto" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">CHEESY GARLIC BROCCOLI</h5>
+                                        <p class="card-text">Broccoli never tasted so good! This cheesy garlic broccoli is a quick and easy side dish that is packed with flavor and nutrition.</p>
+                                        <a href="#" class="btn btn-primary">view</a>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+
+                                <div class="card" >
+                                    <img src="https://assets.bonappetit.com/photos/58b88d89f1c9092f92c4400d/1:1/w_2560%2Cc_limit/nostalgia-easy-mac-cheese.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">MAC AND CHEESE</h5>
+                                        <p class="card-text">Nothing says comforting and classic like mac and cheese.Ooey, gooey, and oh-so-delightful.</p>
+                                        <a href="#" class="btn btn-primary">view</a>
+                                    </div>
+                                </div>
+
+
+
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+
+                                <div class="card" >
+                                    <img src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/c9a1edb5a50d467b8855b92b03db1299/BFV32433_3-IngredientBreakfasts-FB1080.jpg?resize=600:*&output-format=auto&output-quality=auto" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">CHOCOLATE AVOCADO SMOOTHIE</h5>
+                                        <p class="card-text">Chocolate avocado smoothie is a healthy and delicious way to satisfy your sweet tooth. Made with creamy avocado, rich chocolate, and almond milk, this sipper is sure to become your new favorite breakfast or snack.</p>
+                                        <a href="#" class="btn btn-primary">view</a>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+
+
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default ViewRecipe
